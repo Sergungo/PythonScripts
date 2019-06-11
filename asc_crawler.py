d218909489b5da5fd0be16eb7c8d7b29fdeedb91
Sergey Golenko <serjgolenko@gmail.com>
2019-06-11 21:40:21 +0300

    modified

--- asc_crawler.py
+++ asc_crawler.py
@@ -15,66 +15,47 @@ def get_html(url):
 def get_all_rows(html):
 
     soup = BeautifulSoup(html, 'lxml')
-    # trs = soup.find('table').find_all('tr')
+
+    data = []
 
     trs = soup.find('table').find_all('tr')
 
-    rows = []
-
-    print(len(trs))
+    trs.pop(0)
 
     for tr in trs:
 
         tds = tr.find_all('td')
-        # print(len(tds))
 
-        try:
-            while len(tds) > 0:
-                asx_code = tds[0].text.strip()
+        for td in tds:
+
+            if len(td.text.strip()) == 3:
+                asx_code = td.text.strip()
                 print(asx_code)
-                date = tds[1].text.strip()
-                print(date)         
-                # print(tds[3])
-                match = re.search(r'">\s+([^?]+?)<br', tds[3])
-                if match:
-                    print(headline.group(0))
-        except:
-            axc_code = ''
 
-    # data = {'asx_code': asx_code, 'price': price}
+            if re.search(r'(\d{2}\/\d{2}\/\d{4})', str(td)):
+                date = re.sub("^\s+|\n|\r|\s+$", ' ', td.text.strip())
+                print(date)
 
-    return rows
+            match = re.search(r'\">\s+([^?]+?)<br', str(td))
+            if match:
+                headline = match.group(1)
+                print(headline)
 
+            # print(asx_code + ' ' + date + ' ' + headline)
 
-# def get_page_data(html):
-#     soup = BeautifulSoup(html, 'lxml')
+            data.append ({'Asx_code': asx_code, 'Date': date, 'Headline': headline} )
 
-#     try:
-#         asc_codes = soup.find('table').find_all('tr', class_='altrow')  #.find('td').text.strip()
+    return data
 
 
-#         print(asc_codes)
+def write_csv(data):
+    with open('asxcodes.csv', 'a') as file:
+        writer = csv.writer(file)
 
-#     except:
-#         asc_code = ''
+        writer.writerow((data['Asx_code'], data['Date'], data['Headline']))
 
-    # try:
-    #     date = soup.find('table').find('tr', class_='').find('td').text.strip()
-    #     # print(date)
+        # print(data['Asx_code'], 'parsed')
 
-    # except:
-    #     date = ''
-
-    # data = {'AXC CODE': asc_code}  #, 'Date': date
-    # return data
-
-# def write_csv(data):
-#     with open('coinmarketcap.csv', 'a') as file:
-#         writer = csv.writer(file)
-
-#         writer.writerow((data['name'], data['price']))
-
-#         print(data['name'], 'parsed')
 
 def main():
 
@@ -83,6 +64,10 @@ def main():
     html = get_html(url)
 
     data = get_all_rows(html)
+
+    # print(data.values())
+
+    # write_csv(data)
 
 
 if __name__ == "__main__":
--- crawler.py
+++ crawler.py
@@ -1,74 +1,41 @@
-import requests
-
 from bs4 import BeautifulSoup
-
 import csv
-
-from datetime import datetime
-
-
-def get_html(url):
-    resp = requests.get(url)  # response
-    return resp.text
-
-
-def get_all_links(html):
-
-    soup = BeautifulSoup(html, 'lxml')
-    tds = soup.find('table', id='currencies-all').find_all('td',
-                                                           class_='currency-name')
-    links = []
-    for td in tds:
-        a = td.find('a', class_='currency-name-container').get('href').strip()
-        link = 'https://coinmarketcap.com' + a
-        # print(link)
-        links.append(link)
-
-    return links
-
-
-def get_page_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-
-    try:
-        name = soup.find('h1', class_='text-large').text.strip()
-    except:
-        name = ''
-
-        try:
-            price = soup.find('span', id='quote-price').text.strip()
-        except:
-            price = ''
-
-    data = {'name': name, 'price': price}
-    return data
-
-
-def write_csv(i, data):
-    with open('coinmarketcap.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow((data['name'],
-                         data['price']))
-        print(i, data['name'], 'parsed')
-
-
-def main():
-    start = datetime.now()
-
-    url = 'https://coinmarketcap.com/all/views/all/'
-
-    all_links = get_all_links(get_html(url))
-
-    for i, link in enumerate(all_links):
-        html = get_html(link)
-        data = get_page_data(html)
-        write_csv(i, data)
-
-    end = datetime.now()
-
-    total = end - start
-    print(str(total))
-
-
-if __name__ == "__main__":
-    main()
+  
+def parse(html):#собираем данные со страницы
+    soup = BeautifulSoup(html)
+  
+    teams = []
+  
+    for row in soup.select('tbody > tr'):
+        cols = row.select('td')
+  
+        teams.append({
+            'Место': cols[0].text,
+            'Команда': [name.text for name in row.select('a[class=name]')],
+            'Матчи': cols[2].text
+        })
+  
+    return teams
+  
+def save(teams,path):   #ФУНКЦИЯ ЗАПИСИ СПАРСЕННЫХ ФАЙЛОВ В CSV ФАЙЛ
+    with open(path,'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(('Место','Команда','Матчи'))
+  
+        for team in teams:
+            writer.writerow((team['Место'],team['Команда'],team['Матчи']))
+  
+  
+if __name__ == '__main__':
+    url = 'https://www.sports.ru/epl/table/'
+  
+    import urllib.request
+    with urllib.request.urlopen(url) as rs:
+        html = rs.read()
+  
+    teams = parse(html)
+  
+    save(teams,'апл.csv')
+  
+    for team in teams:
+        print(team)
